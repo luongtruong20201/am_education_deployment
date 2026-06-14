e7a4074290a2bd9a6d3cb5bf73e8166eae4a11c6
luongtruong20201 <luongtruong20201@gmail.com>
2026-06-14 19:52:54 +0700

    feature: add .gitignore

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,12 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-docker-compose up -d mysql redis
+docker compose up -d mysql redis
+
+openssl genpkey -algorithm RSA -out private.pem -pkeyopt rsa_keygen_bits:2048
+openssl rsa -pubout -in private.pem -out public.pem
+
 
 sleep 5
 
-docker-compose up -d
+docker compose up
